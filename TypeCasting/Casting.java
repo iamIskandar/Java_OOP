73a74dab5265f281a6b53b09a658b54390638c8c
Irfan Iskandar <iskandar1999@graduate.utm.my>
2021-02-26 02:26:44 +0800

    Add files via upload

---
+++ TypeCasting/Casting.java
@@ -0,0 +1,21 @@
+public class Casting
+{
+	public static void main(String[] args) {
+
+		double var=15.25;
+		int var1=(int)var;
+		int val=20;
+		float val1=(float)val;
+
+		System.out.println(var1);
+		System.out.println(val1);
+
+		String number="20";
+		int x=Integer.parseInt(number);
+
+		System.out.println(x);
+
+
+
+	}
+}
